1ac414bf1a757aadb863ec2ce66fdb28bed1b42b
kevin798 <84560519+kevin798@users.noreply.github.com>
2026-01-29 09:50:53 +0700

    mbg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,31 @@
 <!DOCTYPE html>
 <html lang="id">
-
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-
 <body>
 
-    <nav class="navbar navbar-dark bg-dark">
-        <div class="container-fluid">
-            <span class="navbar-brand">Aplikasi Peminjaman Alat</span>
+<div class="d-flex">
 
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button type="submit" class="btn btn-outline-light btn-sm">
-                    Logout
-                </button>
-            </form>
-        </div>
-    </nav>
+    <!-- SIDEBAR KIRI -->
+    <x-sidebar role="{{ auth()->user()->role }}" />
 
+    <!-- KONTEN KANAN -->
+    <div class="flex-fill">
 
-    <div class="container-fluid">
-        <div class="row">
-            <aside class="col-md-2 bg-light min-vh-100 p-3">
-                @yield('sidebar')
-            </aside>
-            <main class="col-md-10 p-4">
-                @yield('content')
-            </main>
-        </div>
+        <!-- HEADER -->
+        @include('layouts.topbar')
+
+        <!-- CONTENT -->
+        <main class="p-4">
+            @yield('content')
+        </main>
+
     </div>
 
-</body>
+</div>
 
+</body>
 </html>
